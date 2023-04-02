475f9b2f235b8ceb842f315e7b896f86107fef72
Duaa3 <124520079+Duaa3@users.noreply.github.com>
2023-04-02 07:39:15 +0400

    Delete In-M-S/In-M-S directory

--- In-M-S/In-M-S/src/main/java/com/InstituteManagementSystem/InMS/Model/Teacher.java
+++
@@ -1,7 +0,0 @@
-package com.InstituteManagementSystem.InMS.Model;
-// Defining Teacher class with four public instance variables
-public class Teacher {
-    public int id;
-    public String name;
-    public String email;
-}
